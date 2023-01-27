6da1a5116c95e676f3ae6d5178d08dc095cd6d2d
Jenifer Otero <jeniferoteroguillen@gmail.com>
2023-01-27 10:45:15 -0600

    app

---
+++ 01-my-app/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+const nombre = 'Jenifer'
+function App() {
+  return (
+    <div className="App">
+      <h1> Hola mundo </h1>   
+      <h2> Mi nombre es {nombre} </h2>
+    </div>
+  );
+}
+
+export default App;
